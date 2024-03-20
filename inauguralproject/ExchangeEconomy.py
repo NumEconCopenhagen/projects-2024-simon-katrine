fab891943c906577a5b9758a29214f667e9046bd
AnneKatrineLandt <ztd943@alumni.ku.dk>
2024-03-20 19:15:32 +0100

    test2

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -14,10 +14,8 @@ class ExchangeEconomyClass:
         par.w1A = 0.8
         par.w2A = 0.3
 
-        # c. solution 
-        #sol=self.sol=SimpleNamespace()
-
-        #sol.p2=1 # numaire 
+        # c. vector
+        par.p2=1
 
 
     def utility_A(self,x1A,x2A):
@@ -39,36 +37,36 @@ class ExchangeEconomyClass:
         par = self.par
         return par.alpha*((p1*par.w1A+par.p2*par.w2A)/p1)
     
-    def demand_A2(self,p2):
+    def demand_A2(self,p1):
         """ Consumer A's demand for good 2 """
 
         par = self.par
-        return (1-par.alpha)*((p1*par.w1A+p2*par.w2A)/p2)
+        return (1-par.alpha)*((p1*par.w1A+par.p2*par.w2A)/par.p2)
 
     def demand_B1(self,p1):
         """ Consumer B's demand for good 1 """
 
         par = self.par
-        return par.beta*((p1*(1-par.w1A)+p2*(1-par.w2A))/p1)
+        return par.beta*((p1*(1-par.w1A)+par.p2*(1-par.w2A))/p1)
     
-    def demand_B2(self,p2):
+    def demand_B2(self,p1):
         """ Consumer B's demand for good 2 """
 
         par = self.par
-        return (1-par.beta)*((p1*(1-par.w1A)+p2*(1-par.w2A))/p2)
+        return (1-par.beta)*((p1*(1-par.w1A)+par.p2*(1-par.w2A))/par.p2)
 
-    def check_market_clearing(self,p1,p2):
+    def check_market_clearing(self,p1):
 
         par = self.par
 
         #x1A,x2A = self.demand_A1(p1)
         #x1B,x2B = self.demand_B1(p1)
         x1A = self.demand_A1(p1)
-        x2A = self.demand_A2(p2)
+        x2A = self.demand_A2(p1)
         x1B = self.demand_B1(p1)
-        x2B = self.demand_B2(p2)
+        x2B = self.demand_B2(p1)
 
-        eps1 = x1A-par.w1A + x1B-(1-par.w1A)
-        eps2 = x2A-par.w2A + x2B-(1-par.w2A)
+        eps1 = x1A - par.w1A + x1B - (1 - par.w1A)
+        eps2 = x2A - par.w2A + x2B - (1 - par.w2A)
 
         return eps1,eps2
