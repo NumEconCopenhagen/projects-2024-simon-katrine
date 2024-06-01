3731461d47ea19acd2bc558c3a1375791e7cbfc5
AnneKatrineLandt <ztd943@alumni.ku.dk>
2024-06-01 17:44:20 +0200

    y

---
+++ modelproject/modelproject.py
@@ -0,0 +1,16 @@
+from types import SimpleNamespace
+
+class modelprojectclass:
+    
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        # paramters
+        par.alpha = 0.33
+        par.s = 0.3
+        par.n = 0.02
+        par.delta = 0.05
+        par.phi = 0.5
+  
+    
